d4330a16e0101e5e219c23120b5b3c295fc1b608
KuroSenshi95 <48104110+KuroSenshi95@users.noreply.github.com>
2019-03-01 15:16:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'MzY1NjIwMzQyNjA3MzE0OTQ1.D1rNWw.GHv3NyGTvY6Z2NPrfb_OYrgO71o';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = 'k!';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyDVBAP1K2Xry5bFx8yN0Bj9_GaRVVGycEw';
